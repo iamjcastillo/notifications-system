157fda243270b49f1e127140bf88c2ac8d6af791
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 17:10:22 +0100

    Create new endpoint to post notifications

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from app.infrastructure.dependency_injection import setup_notification_system
+from app.infrastructure.dtos.dto import NotificationCreationRequest
+
 app = FastAPI(
     title='DDD API',
-    description='A restful API for system notification using Domain-Driven Design principles.',
+    description='A restful API for system handlers using Domain-Driven Design principles.',
     version='0.0.1',
 )
 
@@ -10,3 +13,11 @@ app = FastAPI(
 @app.get("/health", tags=["Health"])
 async def health():
     return "Healthy"
+
+
+@app.post("/notifications", tags=["Notifications"])
+async def create_notification(notification_creation_request: NotificationCreationRequest):
+    notification_service = setup_notification_system()
+    notification_service.create_notification(
+        notification_creation_request.topic, notification_creation_request.description
+    )
