2180787df056e611ec4c85deb5a56439475f2f84
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 12:28:28 +0100

    Set up notification handlers
    - Used interface segregation

---
+++ app/domain/handlers/notification_handlers.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from app.domain.events.domain_events import NotificationCreatedEvent
+
+
+class NotificationHandler(ABC):
+    @abstractmethod
+    def handle(self, event: NotificationCreatedEvent) -> None:
+        pass
+
+
+class EmailNotificationHandler(NotificationHandler):
+    @abstractmethod
+    def send_email(self, recipient: str, subject: str, body: str) -> None:
+        pass
+
+
+class SlackNotificationHandler(NotificationHandler):
+    @abstractmethod
+    def send_message(self, channel: str, message: str) -> None:
+        pass
