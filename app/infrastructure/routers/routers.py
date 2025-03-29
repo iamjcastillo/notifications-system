9bbe0c744cc9e6ad42fe9a4106874a1f2d99516a
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 17:43:59 +0100

    Add rate limit

--- app/infrastructure/routers/routers.py
+++ app/infrastructure/routers/routers.py
@@ -1,18 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from app.infrastructure.dependency_injection import setup_notification_system
 from app.infrastructure.dtos.dto import NotificationCreationRequest
 
 router = APIRouter()
+limiter = Limiter(key_func=get_remote_address)
 
 
 @router.get("/health", tags=["Health"])
-async def health():
+@limiter.limit("10/minute")
+async def health(request: Request):
     return "Healthy"
 
 
 @router.post("/notifications", tags=["Notifications"])
-async def create_notification(notification_creation_request: NotificationCreationRequest):
+@limiter.limit("5/minute")
+async def create_notification(
+        request: Request,
+        notification_creation_request: NotificationCreationRequest
+):
     notification_service = setup_notification_system()
     notification_service.create_notification(
         notification_creation_request.topic, notification_creation_request.description
