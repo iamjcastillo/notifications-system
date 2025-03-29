06d507fe18824255cee51936ca7067aec76c1086
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 13:17:28 +0100

    Add service to create notification

---
+++ app/application/services/notification_service.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from app.domain.entities.notification import Notification
+from app.domain.events.event_publisher import EventPublisher
+from app.domain.value_objects.description import Description
+from app.domain.value_objects.topic import Topic
+
+
+class NotificationService(ABC):
+    @abstractmethod
+    def create_notification(self, topic: Topic, description: Description) -> Notification:
+        ...
+
+
+class NotificationServiceImpl(NotificationService):
+    def __init__(self, event_publisher: EventPublisher):
+        self._event_publisher = event_publisher
+
+    def create_notification(self, topic: Topic, description: Description) -> Notification:
+        notification = Notification.create(topic, description, self._event_publisher)
+        return notification
---
+++ app/tests/application/services/test_notification_service.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import MagicMock
+
+from app.application.services.notification_service import NotificationServiceImpl
+from app.domain.entities.notification import Notification
+from app.domain.events.event_publisher import EventPublisher
+from app.domain.value_objects.description import Description
+from app.domain.value_objects.topic import Topic
+
+
+class NotificationServiceTestCase(unittest.TestCase):
+    def test_given_topic_and_description_then_notification_is_created(self):
+        topic = Topic.PRICING
+        description = Description.create("Email description")
+        event_publisher = MagicMock(spec_set=EventPublisher)
+        notification_service = NotificationServiceImpl(event_publisher)
+
+        notification = notification_service.create_notification(topic, description)
+
+        self.assertTrue(isinstance(notification, Notification))
+        self.assertEqual(notification.topic, topic)
+        self.assertEqual(notification.description, description)
