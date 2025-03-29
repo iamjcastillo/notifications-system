4be12d0de3c0c9206955879311176a9169ee1225
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 17:04:06 +0100

    Inject dependencies

---
+++ app/infrastructure/dependency_injection.py
@@ -0,0 +1,20 @@
+from app.application.services.notification_service import NotificationServiceImpl
+from app.domain.events.domain_events import EmailNotificationCreatedEvent, SlackNotificationCreatedEvent
+
+from app.domain.events.event_publisher import EventPublisherImpl
+from app.infrastructure.handlers.email_handler import EmailHandler
+from app.infrastructure.handlers.slack_handler import SlackHandler
+
+
+def setup_notification_system():
+    event_publisher = EventPublisherImpl()
+
+    email_handler = EmailHandler()
+    event_publisher.subscribe(EmailNotificationCreatedEvent, email_handler.handle)
+
+    slack_handler = SlackHandler()
+    event_publisher.subscribe(SlackNotificationCreatedEvent, slack_handler.handle)
+
+    notification_service = NotificationServiceImpl(event_publisher)
+
+    return notification_service
---
+++ app/tests/infrastructure/test_dependency_injection.py
@@ -0,0 +1,19 @@
+import unittest
+
+from app.application.services.notification_service import NotificationServiceImpl
+from app.domain.events.domain_events import EmailNotificationCreatedEvent, SlackNotificationCreatedEvent
+from app.domain.events.event_publisher import EventPublisherImpl
+from app.infrastructure.dependency_injection import setup_notification_system
+
+
+class DITestCase(unittest.TestCase):
+    def test_setup_notification_system(self):
+        notification_service = setup_notification_system()
+
+        assert isinstance(notification_service, NotificationServiceImpl)
+
+        event_publisher = notification_service._event_publisher
+        assert isinstance(event_publisher, EventPublisherImpl)
+
+        assert EmailNotificationCreatedEvent in event_publisher._subscribers
+        assert SlackNotificationCreatedEvent in event_publisher._subscribers
