2b33ea70b932f1f7d048cd97145607be2b604414
Jesus Castillo <castilloizquierdojesus@gmail.com>
2025-03-29 16:33:04 +0100

    Add content creation responsibility to events

--- app/domain/events/domain_events.py
+++ app/domain/events/domain_events.py
@@ -1,4 +1,6 @@
+from abc import ABC
 from dataclasses import dataclass
+from typing import Tuple
 
 from app.domain.value_objects.description import Description
 from app.domain.value_objects.notification_id import NotificationID
@@ -6,7 +8,7 @@ from app.domain.value_objects.topic import Topic
 
 
 @dataclass(frozen=True)
-class DomainEvent:
+class DomainEvent(ABC):
     ...
 
 
@@ -19,9 +21,16 @@ class NotificationCreatedEvent(DomainEvent):
 
 @dataclass(frozen=True)
 class EmailNotificationCreatedEvent(NotificationCreatedEvent):
-    ...
+    def create_email_content(self) -> Tuple[str, str, str]:
+        recipient = "test@gmail.com"
+        subject = f"New Notification: {self.topic.value}"
+        body = f"You have a new notification regarding: {self.description.value}"
+        return recipient, subject, body
 
 
 @dataclass(frozen=True)
 class SlackNotificationCreatedEvent(NotificationCreatedEvent):
-    ...
+    def create_slack_content(self) -> Tuple[str, str]:
+        channel = f"New Notification: {self.topic.value}"
+        message = f"You have a new notification regarding: {self.description.value}"
+        return channel, message
---
+++ app/tests/domain/events/test_domain_events.py
@@ -0,0 +1,31 @@
+import unittest
+
+from app.domain.entities.notification import NotificationID
+from app.domain.events.domain_events import EmailNotificationCreatedEvent, SlackNotificationCreatedEvent
+from app.domain.value_objects.description import Description
+from app.domain.value_objects.topic import Topic
+
+
+class DomainEventTestCase(unittest.TestCase):
+    def test_given_email_notification_created_event_when_creating_content_then_email_content_is_created(self):
+        notification_id = NotificationID.generate()
+        topic = Topic.PRICING
+        description = Description.create("Email description")
+        event = EmailNotificationCreatedEvent(notification_id, topic, description)
+
+        recipient, subject, body = event.create_email_content()
+
+        self.assertEqual(recipient, "test@gmail.com")
+        self.assertEqual(subject, "New Notification: pricing")
+        self.assertEqual(body, "You have a new notification regarding: Email description")
+
+    def test_given_slack_notification_created_event_when_creating_content_then_slack_content_is_created(self):
+        notification_id = NotificationID.generate()
+        topic = Topic.SALES
+        description = Description.create("Slack description")
+        event = SlackNotificationCreatedEvent(notification_id, topic, description)
+
+        channel, message = event.create_slack_content()
+
+        self.assertEqual(channel, "New Notification: sales")
+        self.assertEqual(message, "You have a new notification regarding: Slack description")
